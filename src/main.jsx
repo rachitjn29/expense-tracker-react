2d063958079a6eebc5195bc427ab9f961a3fb790
Rachit <rachitjn29@gmail.com>
2026-09-25 19:21:41 +0530

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/expense-tracker-react">
     <App />
     </BrowserRouter>
   </StrictMode>
